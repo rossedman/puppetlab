9868e644d38800b868b38b7641e2d76c3b16e81d
Ross Edman <rossedman@github.com>
2018-12-04 21:34:32 -0600

    Add Code

--- volumes/code/environments/production/manifests/site.pp
+++ volumes/code/environments/production/manifests/site.pp
@@ -1,6 +1,7 @@
 File { backup => false }
 
 node default {
+  notify { 'hello, world!': }
   file { '/tmp/puppet-in-docker':
     ensure  => present,
     content => 'This file is for demonstration purposes only',
